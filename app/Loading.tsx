96afb5e9e876ed72d427e31776d605ad7103d6b3
Lagacy_code <miraglioenzo93@gmail.com>
2024-01-29 22:00:36 +0100

    responsive : stream page, loader

--- app/Loading.tsx
+++ app/Loading.tsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React from "react";
 
-const Loading = () => {
+const Loading = ({ className }: { className: string }) => {
   return (
-    <div>Loading</div>
-  )
-}
+    <div className={className}>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200">
+        <rect fill="#FFFFFF" stroke="#FFFFFF" stroke-width="2" width="30" height="30" x="25" y="85">
+          <animate
+            attributeName="opacity"
+            calcMode="spline"
+            dur="2"
+            values="1;0;1;"
+            keySplines=".5 0 .5 1;.5 0 .5 1"
+            repeatCount="indefinite"
+            begin="-.4"
+          ></animate>
+        </rect>
+        <rect fill="#FFFFFF" stroke="#FFFFFF" stroke-width="2" width="30" height="30" x="85" y="85">
+          <animate
+            attributeName="opacity"
+            calcMode="spline"
+            dur="2"
+            values="1;0;1;"
+            keySplines=".5 0 .5 1;.5 0 .5 1"
+            repeatCount="indefinite"
+            begin="-.2"
+          ></animate>
+        </rect>
+        <rect fill="#FFFFFF" stroke="#FFFFFF" stroke-width="2" width="30" height="30" x="145" y="85">
+          <animate
+            attributeName="opacity"
+            calcMode="spline"
+            dur="2"
+            values="1;0;1;"
+            keySplines=".5 0 .5 1;.5 0 .5 1"
+            repeatCount="indefinite"
+            begin="0"
+          ></animate>
+        </rect>
+      </svg>
+    </div>
+  );
+};
 
-export default Loading
+export default Loading;
--- app/page.tsx
+++ app/page.tsx
@@ -1,43 +1,46 @@
 "use client";
 import Image from "next/image";
 import styles from "./page.module.css";
-import { TwitchEmbed } from "react-twitch-embed";
+import { TwitchEmbed, TwitchPlayer } from "react-twitch-embed";
 import { FaDiscord, FaYoutube, FaInstagram, FaTwitch } from "react-icons/fa";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Loading from "./Loading";
 export default function Home() {
   const [isLoaded, setIsLoaded] = useState(false);
+
+  function loaded() {
+    setIsLoaded(true);
+  }
   return (
     <main className={styles.main}>
-      <Image className={styles.bgImage} src="/background_image.webp" alt="background thresh personna of league of legends" fill priority />
-      <div>
-        {!isLoaded ? <Loading /> : null}
-        <TwitchEmbed className={styles.stream} onVideoReady={() => setIsLoaded(true)} channel="cptntvn" autoplay allowFullscreen />
-      </div>
-
+      {!isLoaded ? <Loading className={styles.loading} /> : null}
+      <TwitchPlayer className={styles.stream} onLoadCapture={loaded} channel="cptntvn" autoplay />
       <aside className={styles.aside}>
-        <Image src="/pseodo.svg" alt="" width={300} height={300} />
-
+        <Image src="/pseodo.svg" alt="cptntvn illustration logo" width={300} height={90} />
         <ul>
           <li>
-            <a href="#">
+            <a href="https://discord.gg/3GFMDeAk" target="_blank">
               <FaDiscord />
               Discord
             </a>
           </li>
           <li>
-            <a href="#">
+            <a href="https://www.youtube.com/@CPTNTVN" target="_blank">
               <FaYoutube />
               Youtube
             </a>
           </li>
           <li>
-            <FaInstagram />
-            <a href="">Instagram</a>
+            <a href="https://www.instagram.com/cptn_tvn?igsh=MWUyczRxMjI1d2NpMg==" target="_blank">
+              <FaInstagram />
+              Instagram
+            </a>
           </li>
           <li>
-            <FaTwitch />
-            <a href="">Twitch</a>
+            <a href="https://www.twitch.tv/cptntvn" target="_blank">
+              <FaTwitch />
+              Twitch
+            </a>
           </li>
         </ul>
       </aside>
